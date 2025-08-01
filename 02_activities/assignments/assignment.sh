e2a06d2cf41213f855456da4f70a783deb9271b3
Sumit Giri <sumit.giri199@gmail.com>
2025-08-01 16:23:46 -0400

    Q 5 completed

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -42,7 +42,7 @@ mkdir server_logs user_logs event_logs
 cd ../..
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-
+cp data/raw/rawdata/*server*.log data/processed/server_logs/
 # 6. Repeat the above step for user logs and event logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
