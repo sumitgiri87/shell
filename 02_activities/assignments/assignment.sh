adb7aa648cb3474da1166c40253a88ae8b2c15e6
Sumit Giri <sumit.giri199@gmail.com>
2025-08-01 15:43:01 -0400

    partially completed assignment

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -33,8 +33,13 @@ mkdir data
 mkdir data/raw
 mv rawdata/ data/raw/
 # 3. List the contents of the ./data/raw directory
-
+ls data/raw
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+cd data
+mkdir processed
+cd processed
+mkdir server_logs user_logs event_logs
+cd ../..
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 
